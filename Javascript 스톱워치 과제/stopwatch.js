cad8b7f3eec930a70b5eda55a9faf28792127b00
hamo-o <yheel369@gmail.com>
2022-07-06 16:21:26 +0900

    기본기능 + 기록추가기능

--- Javascript 스톱워치 과제/stopwatch.js
+++ Javascript 스톱워치 과제/stopwatch.js
@@ -1,41 +1,71 @@
 const title = document.querySelector("#title");
+
 const start_btn = document.querySelector("#btn-start");
 const stop_btn = document.querySelector("#btn-stop");
 const reset_btn = document.querySelector("#btn-reset");
 
 const mytime = document.querySelector("#watch-time");
+const container = document.querySelector("#record-times");
 
+let time = 0;
 let startTime = 0;
+let endTime = 0;
+let timer = 0;
+let sec = 0;
+let msec = 0;
 
 title.addEventListener("click", () => {
   window.location.reload();
 });
 
-function start() {
-  startTime = Date.now();
-  setInterval(go, 1);
-}
-
 function go() {
-  let time = new Date(Date.now() - startTime);
+  time = new Date(Date.now() - startTime);
 
-  let sec = addZero(time.getSeconds());
-  let msec = addZero(time.getMilliseconds());
+  sec = addZero(time.getSeconds());
+  msec = addZero(parseInt(time.getMilliseconds() / 10));
 
   mytime.innerHTML = `<h1>${sec}:${msec}</h1>`;
 }
 
+function start() {
+  if (endTime == 0) {
+    startTime = Date.now();
+  } else {
+    startTime = Date.now() - endTime;
+  }
+  timer = setInterval(go, 1);
+}
+
 function stop() {
-  mytime.innerHTML = `<h1>${time}</h1>`;
+  endTime = time;
+  clearInterval(timer);
+  mytime.innerHTML = `<h1>${sec}:${msec}</h1>`;
+  addList();
 }
 
 function reset() {
-  stop();
+  clearInterval(timer);
   mytime.innerHTML = `<h1>00:00</h1>`;
 }
 
 function addZero(num) {
-  if (num < 10) return "0" + num;
+  if (num < 10) {
+    return "0" + num;
+  } else {
+    return num;
+  }
+}
+
+function addList() {
+  const newItem = document.createElement("li");
+  newItem.innerHTML = `
+  <div class="record-time">
+        <i class="fa-regular fa-circle"></i>
+        <span>${sec}:${msec}</span>
+        <span></span>
+      </div>
+    `;
+  container.appendChild(newItem);
 }
 
 start_btn.addEventListener("click", start);
