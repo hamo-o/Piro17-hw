811328791615b3ab5cea519c3f56750b59f8dc74
hamo-o <yheel369@gmail.com>
2022-07-08 15:03:32 +0900

    1-3

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,3 +1,17 @@
 num = 0
 
-k = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ')
+class Not123(Exception):
+    def __init__(self):
+        super().__init__('1,2,3 중 하나를 입력하세요')
+
+while True:
+    try:       
+        k = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
+        if k != 1 and k != 2 and k != 3:
+            raise Not123
+        else:
+         break
+    except ValueError:
+        print('정수를 입력하세요')
+    except Not123 as e:
+        print(e)
