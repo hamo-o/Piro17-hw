1c941e21c76113fb31668f58c53847d7db663269
hamo-o <yheel369@gmail.com>
2022-07-08 17:02:57 +0900

    1-8

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,64 +1,45 @@
 num = 0
-flag = 0
 
 class Not123(Exception):
     def __init__(self):
         super().__init__('1,2,3 중 하나를 입력하세요')
 
-while True:
-    while True:
-        try:       
-            a = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
-            if a != 1 and a != 2 and a != 3:
-                raise Not123
-            else:
-                break
-        except ValueError:
-            print('정수를 입력하세요')
-        except Not123 as e:
-            print(e)
+class End(Exception):
+    pass
 
-    for i in range(a):
-        if num == 31:
-            break
-        else:
-            flag = 0
-            print('playerA : {0}'.format(num + 1))
-            num += 1
+def brGame(player):
+    global num
     
-    if num == 31:
-        if flag == 1:
-            print('playerA win!')
-            break
-        else:
-            print('playerB win!')
-            break
-        
-    while True:
-        try:       
-            b = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
-            if b != 1 and b != 2 and b != 3:
-                raise Not123
-            else:
-                break
-        except ValueError:
-            print('정수를 입력하세요')
-        except Not123 as e:
-            print(e)
+    try:       
+        k = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
+        if k != 1 and k != 2 and k != 3:
+            raise Not123
+    except ValueError:
+        print('정수를 입력하세요')
+    except Not123 as e:
+        print(e)
 
-    for i in range(b):
+    for i in range(k):
         if num == 31:
             break
         else:
-            flag = 1
-            print('playerB : {0}'.format(num + 1))
+            print('player{0} : {1}'.format(player, num + 1))
             num += 1
-        
-    if num != 31:
-        pass
-    elif flag == 1:
-        print('playerA win!')
+            
+    if num == 31:
+        if player == a:
+            print('playerB win!')
+        else:
+            print('playerA win!')
+        raise End
+            
+a = 'A'
+b = 'B'
+
+while True:
+    try:
+        brGame(a)
+        brGame(b)
+    except End:
         break
-    else:
-        print('playerB win!')
-        break
+
