a09afbe2db3dff183a176e19b053b6282fe883bd
hamo-o <yheel369@gmail.com>
2022-07-15 00:40:57 +0900

    Create, Read, Update 기능 구현

--- myMovieReviews/movie/urls.py
+++ myMovieReviews/movie/urls.py
@@ -6,4 +6,7 @@ app_name = "movie"
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('create', views.create, name="create"),
+    path('review/<int:id>', views.detail, name="detail"),
+    path('update/<int:id>', views.update, name="update"),
 ]
--- myMovieReviews/movie/views.py
+++ myMovieReviews/movie/views.py
@@ -1,5 +1,6 @@
 from multiprocessing import context
-from django.shortcuts import render
+from turtle import title
+from django.shortcuts import redirect, render
 from .models import Movie
 
 # Create your views here.
@@ -13,5 +14,51 @@ def home(request):
     return render(request, template_name="movie/home.html", context=context)
 
 def create(request):
-    pass
+    if request.method == "POST":
+        mytitle = request.POST["title"]
+        myyear = request.POST["year"]
+        mytype = request.POST["type"]
+        mystar = request.POST["star"]
+        mytime = request.POST["time"]
+        myreview = request.POST["review"]
+        mydirector = request.POST["director"]
+        myactor = request.POST["actor"]
+        
+        Movie.objects.create(title=mytitle, year=myyear, type=mytype, 
+star=mystar, time=mytime, review=myreview, director=mydirector, actor=myactor)
+        return redirect('/')
     
+    context = {}
+    return render(request, template_name="movie/create.html", context=context)
+
+def detail(request, id):
+    movie = Movie.objects.get(id=id)
+    
+    context = {
+        "movie": movie
+    }
+    
+    return render(request, template_name="movie/detail.html", context=context)
+
+def update(request, id):
+    if request.method == "POST":
+        mytitle = request.POST["title"]
+        myyear = request.POST["year"]
+        mytype = request.POST["type"]
+        mystar = request.POST["star"]
+        mytime = request.POST["time"]
+        myreview = request.POST["review"]
+        mydirector = request.POST["director"]
+        myactor = request.POST["actor"]
+        
+        Movie.objects.filter(id=id).update(title=mytitle, year=myyear, type=mytype, 
+star=mystar, time=mytime, review=myreview, director=mydirector, actor=myactor)
+        return redirect(f"/review/{id}")
+    
+    elif request.method == "GET":
+        movie = Movie.objects.get(id=id)
+        context = {
+            "movie": movie
+        }
+        
+    return render(request, template_name='movie/update.html', context=context)
